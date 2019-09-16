ebb81324b6432a390cdb144f53bc4b04a26ee5f2
xwjie <121509092@qq.com>
2019-09-16 21:58:15 +0800

    完成

---
+++ README.md
@@ -0,0 +1,25 @@
+# 生成markdown链接文本插件
+
+Github：[xwjie/chrome-copy-markdown-link-plug](https://github.com/xwjie/chrome-copy-markdown-link-plug)
+
+## 安装
+
+`扩展程序`里面打开`开发者模式`，点击`加载已解压的扩展程序`, 直接选择 `source` 目录安装。
+
+点击右键：
+
+![效果](snap.png)
+
+结果：
+
+```
+[xwjie (晓风轻)](https://github.com/xwjie)
+```
+
+## 参考 
+
+[【干货】Chrome插件(扩展)开发全攻略 - 我是小茗同学 - 博客园](https://www.cnblogs.com/liuxianan/p/chrome-plugin-develop.html#%E5%AE%98%E6%96%B9%E8%B5%84%E6%96%99)
+
+https://github.com/bumble-org/clipboard
+
+> 2019.9.16
---
+++ source/background.js
@@ -0,0 +1,63 @@
+// background.js
+
+
+/**
+ * 复制文本
+ * 参考：[bumble-org/clipboard: Use the clipboard with ease in Chrome extensions.](https://github.com/bumble-org/clipboard)
+ * @param {*} str 
+ */
+function copyText(str) {
+	// Create hidden input with text
+	const el = document.createElement('textarea');
+	el.value = str;
+	document.body.append(el);
+
+	// Select the text and copy to clipboard
+	el.select();
+	const success = document.execCommand('copy');
+	el.remove();
+
+	return success ?
+		Promise.resolve(str) :
+		Promise.reject(new Error('Unable to write to clipboard'));
+}
+
+/**
+ * 显示提示框
+ * @param {*} str 
+ * @param {*} title 
+ */
+function msgbox(str, title) {
+	chrome.notifications.create(null, {
+		type: 'basic',
+		iconUrl: 'img/icon.png',
+		title: title || 'Info',
+		message: str
+	});
+}
+
+/**
+ * 拼接markdown链接文本
+ * tab属性见：[chrome.tabs - Google Chrome](https://developer.chrome.com/extensions/tabs#type-Tab)
+ * @param {*} tab 
+ */
+function extractLink(tab) {
+	let title = tab.title;
+	let url = tab.url;
+
+	return  `[${title}](${url})`;
+}
+
+chrome.contextMenus.create({
+	title: "Copy MarkDown Link",
+	onclick: function () {
+		chrome.tabs.getSelected(function (tab) {
+				console.dir(tab);
+				// chrome.tabs.sendRequest(tab.id,jsonData, callBack);
+				copyText(extractLink(tab))
+					.then(msgbox('Copy Success!'));
+			})
+		}
+	});
+
+	
